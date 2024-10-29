fadac51af48e425467c1f0d293c57d6c871c9a79
Zaza Pipia <zaza.pipia.023@gmail.com>
2024-10-29 21:11:24 +0300

    Created flyway migrations and added test data

---
+++ src/main/resources/db/migration/V00002__Add_test_data_to_person.sql
@@ -0,0 +1,3 @@
+INSERT INTO person(name, password) VALUES ('zaza', 'zpassword');
+INSERT INTO person(name, password) VALUES ('alina', 'apassword');
+INSERT INTO person(name, password) VALUES ('egor', 'epassword');
